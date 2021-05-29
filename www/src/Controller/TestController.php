92bb770e694ef2a5b47819b019bed989101a9600
Pablo Sebastian Martinez <pablosmartinez123@gmail.com>
2021-05-29 20:28:43 -0300

    First commit

---
+++ www/src/Controller/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController
+{
+     /**
+      * @Route("/test")
+      */
+      public function number()
+      {
+          return new Response("hola2");
+      }
+}
